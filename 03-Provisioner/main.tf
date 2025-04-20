cf7ed3b8fdf64c8ae09e2fabbe01bb2a0fda77a9
Adrian Rubico <adrianrubico@icloud.com>
2025-04-20 15:45:59 +0800

    Update 03-Provisioner

--- 03-Provisioner/main.tf
+++ 03-Provisioner/main.tf
@@ -6,7 +6,6 @@ resource "azurerm_resource_group" "rg-example" {
   }
 }
 
-
 resource "azurerm_storage_account" "sa-example" {
   name                     = "stgacsample${local.env}01"
   resource_group_name      = azurerm_resource_group.rg-example.name
@@ -48,6 +47,16 @@ resource "azurerm_linux_virtual_machine" "vm-example" {
   }
 }
 
+resource "azurerm_public_ip" "pip-example" {
+  name                = "vm-public-ip"
+  resource_group_name = azurerm_resource_group.rg-example.name
+  location            = azurerm_resource_group.rg-example.location
+  allocation_method   = "Static"
+  tags = {
+    environment = local.env
+  }
+}
+
 resource "azurerm_virtual_network" "vnet-example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
@@ -71,5 +80,33 @@ resource "azurerm_network_interface" "nic-example" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet-example.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-example.id
   }
+}
+
+resource "azurerm_network_security_group" "nsg-example" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.rg-example.location
+  resource_group_name = azurerm_resource_group.rg-example.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = local.env
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.subnet-example.id
+  network_security_group_id = azurerm_network_security_group.nsg-example.id
 }
