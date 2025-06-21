a01e8378d87131129cb861c6ce084baed772a519
Adrian Rubico <adrianrubico@icloud.com>
2025-06-21 20:59:39 +0800

    Update 07-modules

--- 07-Modules/main.tf
+++ 07-Modules/main.tf
@@ -5,15 +5,3 @@ resource "azurerm_resource_group" "rg-example" {
     environment = local.env
   }
 }
-
-resource "azurerm_storage_account" "sa-example" {
-  name                     = "stgacsample${local.env}01"
-  resource_group_name      = azurerm_resource_group.rg-example.name
-  location                 = azurerm_resource_group.rg-example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = local.env
-  }
-}
